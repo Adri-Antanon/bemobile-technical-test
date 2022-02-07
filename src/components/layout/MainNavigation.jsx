bfbdbb1ca7590fae53137798ac13b2cc9832afac
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-02-07 02:27:55 +0100

    style: add breadcrumbs

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -4,16 +4,30 @@ import styles from './MainNavigation.module.css';
 
 const MainNavigation = () => (
   <header className={styles.header}>
-    <div className={styles.logo}>
-      <NavLink
-        to="/products"
-        className={(navData) => (navData.isActive ? styles.active : '')}
-      >
-        Bemobile
-      </NavLink>
+    <div>
+      <div className={styles.logo}>
+        <NavLink
+          to="/products"
+          className={(navData) => (navData.isActive ? styles.active : '')}
+        >
+          Bemobile
+        </NavLink>
+      </div>
+      <nav className={styles.nav}>
+        <p>CARRITO: 0</p>
+      </nav>
     </div>
-    <nav className={styles.nav}>
-      <p>CARRITO: 0</p>
+    <nav className={styles.breadcrumb}>
+      <ul>
+        <li>
+          <NavLink
+            to={`/products/${12434}`}
+            className={(navData) => (navData.isActive ? styles.active : '')}
+          >
+            Test
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   </header>
 );
