f48d95326d15d18b0af6653046cf52abb8297519
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-02-08 12:54:04 +0100

    feat: use local storage to save products

---
+++ src/components/helpers/expirationTime.js
@@ -0,0 +1,11 @@
+const getExpirationDate = () => {
+  const hourInMiliseconds = 60 * 60 * 1000;
+  const expirationDate = new Date().getTime() + hourInMiliseconds;
+
+  const today = new Date().getTime();
+  return { expirationDate, today };
+};
+
+export default getExpirationDate;
+
+// date:1644324460895
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,4 +1,5 @@
 import { useMemo, useCallback, useState, useEffect } from 'react';
+import getExpirationDate from '../components/helpers/expirationTime';
 
 import config from '../config/constants';
 
@@ -9,15 +10,32 @@ const useProducts = (search) => {
   const fetchProductsHandler = useCallback(async () => {
     setIsLoading(true);
 
+    const { expirationDate, today } = getExpirationDate();
+
     try {
-      const response = await fetch(`${config.API_URL}/product`);
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
+      const products = await JSON.parse(localStorage.getItem('products'));
+
+      if (!products || today > products.date) {
+        const response = await fetch(`${config.API_URL}/product`);
+        if (!response.ok) {
+          throw new Error('Something went wrong!');
+        }
+
+        const devicesData = await response.json();
+        localStorage.setItem(
+          'products',
+          JSON.stringify({
+            date: expirationDate,
+            value: devicesData,
+          }),
+        );
+
+        setProductList(devicesData);
+        setIsLoading(false);
+
+        return;
       }
-
-      const devicesData = await response.json();
-
-      setProductList(devicesData);
+      setProductList(products.value);
     } catch (error) {
       throw new Error(error.message);
     }
@@ -40,3 +58,37 @@ const useProducts = (search) => {
 };
 
 export default useProducts;
+
+/*
+Usar locaslStorage, seteamos la fecha que queremos como límite con el tomorrow
+const tomorrow = new Date().getTime() + 1 * 24 * 60 * 60 * 1000;
+      const today = new Date().getTime();
+      // REFACTOR
+      // Copiar en todos los widgets de la home que tenga que cambiar los facets a llamadas únicas
+
+      try {
+        // ALL BARS ON THE DDBB
+        const localTotalBars = await JSON.parse(
+          await localStorage.getItem("widgetBarsTotalBars")
+        );
+        if (!localTotalBars || today > localTotalBars.date) {
+          const totalBars = await API.aggregation({
+            endpoint: "/bars",
+            multiple: true,
+            query: {
+              agg: JSON.stringify([{ $count: "count" }]),
+            },
+          });
+          dataBars.sectors[3].value = totalBars[0].count;
+          await localStorage.setItem(
+            "widgetBarsTotalBars",
+            await JSON.stringify({
+              date: tomorrow,
+              value: totalBars[0].count,
+            })
+          );
+        } else {
+          dataBars.sectors[3].value = localTotalBars.value;
+        }
+
+*/
