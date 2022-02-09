361ceebf4af2c993633df9baa18e4e014caf0e2f
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-02-09 03:24:44 +0100

    feat: add custom hook useProductDetail

--- src/components/product/ProductDescription.jsx
+++ src/components/product/ProductDescription.jsx
@@ -3,12 +3,11 @@ import styles from './ProductDescription.module.css';
 const ProductDescription = ({ product }) => (
   <article className={styles.description}>
     <h3>Description</h3>
-    <ul />
-    {Object.entries(product).map(([key, value]) => (
-      <li key={key}>
-        {`${key}: ${value}`}
-      </li>
-    ))}
+    <ul>
+      {Object.entries(product).map(([key, value]) => (
+        <li key={key}>{`${key}: ${value || 'no data'}`}</li>
+      ))}
+    </ul>
   </article>
 );
 
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,90 +1,18 @@
-import { useState, useCallback, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
 import ProductDescription from '../components/product/ProductDescription';
 import ProductImage from '../components/product/ProductImage';
 import DetailContainer from '../components/UI/DetailContainer';
-
-import getExpirationDate from '../components/helpers/expirationTime';
-import config from '../config/constants';
 import LoadingSpinner from '../components/UI/LoadingSpinner';
 
-const ProductDetailsPage = () => {
-  const [productDetail, setProductDetail] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+import useProductDetail from '../hooks/useProductDetail';
 
+const ProductDetailsPage = () => {
   const params = useParams();
 
-  const fetchProductDetailHandler = useCallback(async () => {
-    setIsLoading(true);
-
-    const { expirationDate, today } = getExpirationDate();
-
-    try {
-      const product = await JSON.parse(localStorage.getItem(params.productId));
-
-      if (!product || today > product.date) {
-        const response = await fetch(
-          `${config.API_URL}/product/${params.productId}`,
-        );
-        if (!response.ok) {
-          throw new Error('Something went wrong!');
-        }
-
-        const deviceData = await response.json();
-        localStorage.setItem(
-          params.productId,
-          JSON.stringify({
-            date: expirationDate,
-            value: deviceData,
-          }),
-        );
-
-        setProductDetail(deviceData);
-        setIsLoading(false);
-
-        return;
-      }
-      setProductDetail(product.value);
-    } catch (error) {
-      throw new Error(error.message);
-    }
-
-    setIsLoading(false);
-  }, []);
-
-  useEffect(() => {
-    fetchProductDetailHandler();
-  }, [fetchProductDetailHandler]);
-
-  const {
-    brand,
-    model,
-    price,
-    cpu,
-    ram,
-    os,
-    displayResolution,
-    battery,
-    primaryCamera,
-    secondaryCmera,
-    dimentions,
-    weight,
-  } = productDetail;
-
-  const productDescription = {
-    brand,
-    model,
-    price,
-    cpu,
-    ram,
-    os,
-    displayResolution,
-    battery,
-    primaryCamera,
-    secondaryCmera,
-    dimentions,
-    weight,
-  };
+  const { isLoading, productDescription, productDetail } = useProductDetail(
+    params.productId,
+  );
 
   if (isLoading) {
     return (
